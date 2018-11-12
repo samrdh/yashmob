68447c23dd1df597a136c436132993b63393ac7f
samrdh <samrdhgoa@gmail.com>
2018-11-12 18:22:33 +0530

    font & color changed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,16 +15,19 @@ export class HomePage {
       title: "Welcome to YASH",
       description: "a FUN & EASY way to get DISCOUNTS in selected stores.",
       image: "assets/imgs/yashgroupgoa.png",
+      color:"#8dae37",
     },
     {
       title: "Step 1",
       description: "Register & Login with your mobile number to get started",
       image: "assets/img/ica-slidebox-img-2.png",
+      color:"#dbae0a",
     },
     {
       title: "Step 2",
       description: "Select a store to view or redeem points",
       image: "assets/img/ica-slidebox-img-3.png",
+      color:"#3fa6a0",
     }
   ];
 
